b7d08271b7033e06062d4e2156dbe176ef07fcef
Gary Willoughby <dev@nomad.uk.net>
2022-04-08 19:11:20 +0100

    Added a binary heap data type.

---
+++ binaryheap/binaryheap.go
@@ -0,0 +1,189 @@
+package binaryheap
+
+import (
+	"golang.org/x/exp/slices"
+)
+
+// BinaryHeap is the main heap type.
+type BinaryHeap[T comparable] struct {
+	data   []T
+	pred   func(a T, b T) bool
+	sorted bool
+}
+
+// New is used to create a new heap.
+// The passed function is a predicate that returns true if the first parameter
+// is greater than the second. This predicate defines the sorting order between
+// the heap items and is called during insertion and extraction.
+func New[T comparable](pred func(a T, b T) bool) BinaryHeap[T] {
+	return BinaryHeap[T]{
+		data: make([]T, 0, 16),
+		pred: pred,
+	}
+}
+
+// Count returns the amount of entries in the heap.
+func (b BinaryHeap[T]) Count() int {
+	return len(b.data)
+}
+
+// Empty returns true if the heap is empty, false if not.
+func (b BinaryHeap[T]) Empty() bool {
+	return b.Count() == 0
+}
+
+// SiftUp sifts the value at the passed index up through the heap until it finds
+// its correct position.
+func (b *BinaryHeap[T]) siftUp(childIndex int) {
+	var parent T
+	var child T
+	var parentIndex int
+
+	if childIndex > 0 {
+		if childIndex > 2 {
+			if childIndex%2 == 0 {
+				parentIndex = (childIndex - 2) / 2
+			} else {
+				parentIndex = (childIndex - 1) / 2
+			}
+		} else {
+			parentIndex = 0
+		}
+
+		parent = b.data[parentIndex]
+		child = b.data[childIndex]
+
+		if b.pred(child, parent) {
+			b.data[parentIndex] = child
+			b.data[childIndex] = parent
+
+			if parentIndex > 0 {
+				b.siftUp(parentIndex)
+			}
+		}
+	}
+}
+
+// SiftDown sifts the value at the passed index down through the heap until it finds
+// its correct position.
+func (b *BinaryHeap[T]) siftDown(parentIndex int) {
+	var parent T
+	var child1 T
+	var child2 T
+	var child1Index int
+	var child2Index int
+
+	child1Index = (2 * parentIndex) + 1
+	child2Index = (2 * parentIndex) + 2
+
+	if b.Count() <= child1Index { // The parent has no children.
+		return
+
+	} else if b.Count() == child2Index { // The parent has one child.
+		parent = b.data[parentIndex]
+		child1 = b.data[child1Index]
+
+		if b.pred(child1, parent) {
+			b.data[parentIndex] = child1
+			b.data[child1Index] = parent
+			b.siftDown(child1Index)
+		}
+
+	} else { // The parent has two children.
+		parent = b.data[parentIndex]
+		child1 = b.data[child1Index]
+		child2 = b.data[child2Index]
+
+		// Compare the parent against the greater child.
+		if b.pred(child1, child2) {
+			if b.pred(child1, parent) {
+				b.data[parentIndex] = child1
+				b.data[child1Index] = parent
+				b.siftDown(child1Index)
+			}
+		} else {
+			if b.pred(child2, parent) {
+				b.data[parentIndex] = child2
+				b.data[child2Index] = parent
+				b.siftDown(child2Index)
+			}
+		}
+	}
+}
+
+// Insert inserts a new value into the heap.
+func (b *BinaryHeap[T]) Insert(val T) {
+	b.data = append(b.data, val)
+	b.siftUp(b.Count() - 1)
+	b.sorted = false
+}
+
+// Peek returns the first value at the top of the heap.
+func (b BinaryHeap[T]) Peek() T {
+	return b.data[0]
+}
+
+// Extract returns and removes the first value from the heap.
+func (b *BinaryHeap[T]) Extract() T {
+	if b.Empty() {
+		panic("binary heap empty, extracting failed")
+	}
+	val := b.data[0]
+	b.data[0] = b.data[b.Count()-1]
+	b.data = b.data[0 : b.Count()-1]
+	b.siftDown(0)
+	b.sorted = false
+	return val
+}
+
+// Contains returns true if the value exists in the heap, false if not.
+func (b BinaryHeap[T]) Contains(val T) bool {
+	for _, v := range b.data {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
+
+// Clear empties the entire heap.
+func (b *BinaryHeap[T]) Clear() {
+	b.data = b.data[:0]
+}
+
+// Sort the heap ready for iterating.
+// The heap needs to be sorted to be iterated correctly using a loop. This is to
+// make sure values are delivered in the correct order. Sorted or unsorted, the
+// heap data structure will always be correct because of the algorithms used
+// when sifting.
+func (b *BinaryHeap[T]) sort() {
+	if !b.sorted {
+		slices.SortFunc(b.data, b.pred)
+		b.sorted = true
+	}
+}
+
+// ForEach iterates over the dataset within the heap, calling the passed
+// function for each value.
+func (b BinaryHeap[T]) ForEach(f func(val T)) {
+	b.sort()
+	for _, v := range b.data {
+		f(v)
+	}
+}
+
+// Values returns the values delivered through a channel. This is safe to be
+// called in a for/range loop as it only creates one channel.
+func (b BinaryHeap[T]) Values() chan T {
+	b.sort()
+	c := make(chan T)
+
+	go func() {
+		for _, v := range b.data {
+			c <- v
+		}
+		close(c)
+	}()
+
+	return c
+}
---
+++ binaryheap/binaryheap_test.go
@@ -0,0 +1,204 @@
+package binaryheap
+
+import (
+	"testing"
+
+	"github.com/nomad-software/goad/assert"
+)
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	b := New(func(a, b int) bool { return a < b })
+	b.Insert(6)
+	b.Insert(5)
+	b.Insert(2)
+	b.Insert(9)
+	b.Insert(4)
+	b.Insert(8)
+	b.Insert(7)
+	b.Insert(1)
+	b.Insert(3)
+	b.Insert(10)
+
+	assert.False(t, b.Empty())
+	assert.Eq(t, b.Count(), 10)
+
+	for i := 1; i <= 10; i++ {
+		assert.Eq(t, b.Peek(), i)
+		assert.Eq(t, b.Extract(), i)
+	}
+
+	assert.True(t, b.Empty())
+	assert.Eq(t, b.Count(), 0)
+}
+
+func TestStruct(t *testing.T) {
+	t.Parallel()
+
+	type Foo struct {
+		Foo int
+		Bar string
+	}
+
+	b := New(func(a, b Foo) bool { return a.Foo < b.Foo })
+
+	f1 := Foo{Foo: 2, Bar: "bar"}
+	f2 := Foo{Foo: 4, Bar: "qux"}
+	f3 := Foo{Foo: 3, Bar: "baz"}
+	f4 := Foo{Foo: 1, Bar: "foo"}
+
+	b.Insert(f1)
+	b.Insert(f2)
+	b.Insert(f3)
+	b.Insert(f4)
+
+	assert.False(t, b.Empty())
+	assert.Eq(t, b.Count(), 4)
+
+	assert.True(t, b.Contains(f1))
+	assert.True(t, b.Contains(f2))
+	assert.True(t, b.Contains(f3))
+	assert.True(t, b.Contains(f4))
+
+	assert.Eq(t, b.Extract(), f4)
+	assert.Eq(t, b.Extract(), f1)
+	assert.Eq(t, b.Extract(), f3)
+	assert.Eq(t, b.Extract(), f2)
+
+	assert.True(t, b.Empty())
+}
+
+func TestLargeCapacity(t *testing.T) {
+	t.Parallel()
+
+	b := New(func(a, b int) bool { return a < b })
+	limit := 1_000_000
+
+	for i := 1; i <= limit; i++ {
+		b.Insert(i)
+		assert.Eq(t, b.Peek(), 1)
+		assert.Eq(t, b.Count(), i)
+	}
+
+	assert.Eq(t, b.Peek(), 1)
+	assert.Eq(t, b.Count(), limit)
+	assert.True(t, b.Contains(1))
+	assert.True(t, b.Contains(limit))
+	assert.False(t, b.Empty())
+
+	c := b.Count()
+	for i := 1; i <= limit; i++ {
+		assert.Eq(t, b.Count(), c)
+		assert.Eq(t, b.Peek(), i)
+		assert.Eq(t, b.Extract(), i)
+		c--
+	}
+
+	assert.True(t, b.Empty())
+	assert.Eq(t, b.Count(), 0)
+}
+
+func TestFailedExtract(t *testing.T) {
+	t.Parallel()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("no panic detected")
+		}
+	}()
+
+	b := New(func(a, b int) bool { return a < b })
+	b.Extract()
+}
+
+func TestContains(t *testing.T) {
+	t.Parallel()
+
+	b := New(func(a, b string) bool { return a < b })
+	b.Insert("foo")
+	b.Insert("bar")
+	b.Insert("baz")
+	b.Insert("qux")
+
+	assert.True(t, b.Contains("bar"))
+	assert.False(t, b.Contains("fuz"))
+}
+
+func TestClearing(t *testing.T) {
+	t.Parallel()
+
+	b := New(func(a, b string) bool { return a < b })
+	b.Insert("foo")
+	b.Insert("bar")
+	b.Insert("baz")
+	b.Insert("qux")
+	assert.Eq(t, b.Count(), 4)
+
+	b.Clear()
+	assert.Eq(t, b.Count(), 0)
+
+	b.Insert("foo")
+	b.Insert("bar")
+	assert.Eq(t, b.Count(), 2)
+}
+
+func TestForEach(t *testing.T) {
+	t.Parallel()
+
+	b := New(func(a, b int) bool { return a < b })
+	b.Insert(6)
+	b.Insert(5)
+	b.Insert(2)
+	b.Insert(9)
+	b.Insert(4)
+	b.Insert(8)
+	b.Insert(7)
+	b.Insert(1)
+	b.Insert(3)
+	b.Insert(10)
+
+	assert.False(t, b.Empty())
+	assert.Eq(t, b.Count(), 10)
+
+	i := 1
+	b.ForEach(func(val int) {
+		assert.Eq(t, val, i)
+		i++
+	})
+
+	assert.False(t, b.Empty())
+	assert.Eq(t, b.Count(), 10)
+
+	b.Clear()
+	b.ForEach(func(val int) {
+		t.Errorf("stack not cleared")
+	})
+}
+
+func TestValues(t *testing.T) {
+	t.Parallel()
+
+	b := New(func(a, b int) bool { return a < b })
+	b.Insert(6)
+	b.Insert(5)
+	b.Insert(2)
+	b.Insert(9)
+	b.Insert(4)
+	b.Insert(8)
+	b.Insert(7)
+	b.Insert(1)
+	b.Insert(3)
+	b.Insert(10)
+
+	i := 1
+	for val := range b.Values() {
+		assert.Eq(t, val, i)
+		i++
+	}
+
+	b.Clear()
+	for range b.Values() {
+		t.Errorf("queue not cleared")
+	}
+}
